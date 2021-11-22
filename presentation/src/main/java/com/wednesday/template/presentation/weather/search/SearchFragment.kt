58a5c65e69b82b32eca689cbde9cdad0c057e0bd
manav <manav@wednesday.is>
2021-11-22 11:35:01 +0530

    search fragment changes commit.

--- presentation/src/main/java/com/wednesday/template/presentation/weather/search/SearchFragment.kt
+++ presentation/src/main/java/com/wednesday/template/presentation/weather/search/SearchFragment.kt
@@ -1,49 +1,68 @@
 package com.wednesday.template.presentation.weather.search
 
-import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.annotation.CallSuper
+import androidx.fragment.app.viewModels
+import com.wednesday.template.presentation.base.effect.Effect
+import com.wednesday.template.presentation.base.fragment.BaseFragment
+import com.wednesday.template.presentation.base.fragment.BindingProvider
+import com.wednesday.template.presentation.base.viewmodel.BaseViewModel
+import com.wednesday.template.presentation.screen.Screen
+import com.wednesday.template.presentation.screen.ScreenState
 import com.wednesday.template.resources.databinding.FragmentSearchBinding
 import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class SearchFragment : Fragment() {
+class SearchFragment<
+        SCREEN_STATE:ScreenState> : BaseFragment<FragmentSearchBinding,
+        SearchFragmentScreen,
+        SCREEN_STATE,
+        BaseViewModel<SearchFragmentScreen,SCREEN_STATE>>()
+{
 
-    private lateinit var _binding: FragmentSearchBinding
-    private val binding get() = _binding
+    val vm:SearchFragmentViewModel by viewModels()
 
-    private val viewModel by inject<SearchFragmentViewModel>()
+    @CallSuper
+    override fun onViewCreated(binding: FragmentSearchBinding) {
+        super.onViewCreated(binding)
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        // Inflate the layout for this fragment
-        _binding = FragmentSearchBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
         setOnClickListener(binding)
+
     }
 
     private fun setOnClickListener(binding: FragmentSearchBinding) = with(binding) {
 
-        searchEditText.addTextChangedListener(object : TextWatcher {
+        searchEditText.addTextChangedListener(object :TextWatcher{
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
             }
 
             override fun afterTextChanged(name: Editable?) {
-                viewModel.searchCity(name.toString())
+                vm.searchCity(name.toString())
             }
+
         })
+
     }
+
+
+
+    override fun onEffect(effect: Effect) {
+
+    }
+
+    override fun onState(screenState: SCREEN_STATE) {
+
+    }
+
+    override val viewModel: BaseViewModel<SearchFragmentScreen, SCREEN_STATE> by viewModels()
+
+    override val bindingProvider: BindingProvider<FragmentSearchBinding>
+        get() = TODO("Not yet implemented")
 }
--- presentation/src/main/java/com/wednesday/template/presentation/weather/search/SearchFragmentViewModel.kt
+++ presentation/src/main/java/com/wednesday/template/presentation/weather/search/SearchFragmentViewModel.kt
@@ -4,16 +4,26 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.wednesday.template.interactor.weather.SearchCityInteractor
+import com.wednesday.template.presentation.base.viewmodel.BaseViewModel
+import com.wednesday.template.presentation.screen.ScreenState
 import kotlinx.coroutines.launch
 
 class SearchFragmentViewModel(
     private val searchCityInteractor: SearchCityInteractor
-) : ViewModel() {
+) : BaseViewModel<SearchFragmentScreen, ScreenState>() {
 
     fun searchCity(city: String) {
         viewModelScope.launch {
             val cityList = searchCityInteractor.searchCity(city)
             Log.d("$$$$$$$$$$$$", cityList.toString())
         }
+    }
+
+    override fun getDefaultScreenState(): ScreenState {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreate(fromRecreate: Boolean) {
+        TODO("Not yet implemented")
     }
 }
