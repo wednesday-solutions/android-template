c5d5292d1753b0a190a5a6902fcaab13bf4ccd68
manav <manav@wednesday.is>
2021-11-25 19:49:00 +0530

    Start Screen initial commit-2.

--- presentation/src/main/java/com/wednesday/template/presentation/weather/start/StartFragment.kt
+++ presentation/src/main/java/com/wednesday/template/presentation/weather/start/StartFragment.kt
@@ -1,34 +1,26 @@
 package com.wednesday.template.presentation.weather.start
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.wednesday.template.presentation.base.effect.Effect
 import com.wednesday.template.presentation.base.fragment.BindingProvider
 import com.wednesday.template.presentation.base.fragment.MainFragment
 import com.wednesday.template.presentation.base.toolbar.ToolbarComponent
-import com.wednesday.template.resources.R
 import com.wednesday.template.resources.databinding.FragmentStartBinding
 
 class StartFragment : MainFragment<
-        FragmentStartBinding,
-        StartFragmentScreen,
-        StartScreenState,
-        StartScreenViewModel>(){
-    
+    FragmentStartBinding,
+    StartFragmentScreen,
+    StartScreenState,
+    StartScreenViewModel>() {
+
     override val viewModel: StartScreenViewModel
         get() = TODO("Not yet implemented")
     override val bindingProvider: BindingProvider<FragmentStartBinding>
         get() = TODO("Not yet implemented")
-    
+
     override fun onEffect(effect: Effect) {
         TODO("Not yet implemented")
     }
-    
+
     override val toolbarComponent: ToolbarComponent
         get() = TODO("Not yet implemented")
-    
-    
 }
--- presentation/src/main/java/com/wednesday/template/presentation/weather/start/StartScreenIntent.kt
+++ presentation/src/main/java/com/wednesday/template/presentation/weather/start/StartScreenIntent.kt
@@ -2,9 +2,9 @@ package com.wednesday.template.presentation.weather.start
 
 import com.wednesday.template.presentation.base.intent.Intent
 
-interface StartScreenIntent : Intent{
-    
+interface StartScreenIntent : Intent {
+
     data class FavoriteCities(
         val city: String
-    ): StartScreenIntent
+    ) : StartScreenIntent
 }
