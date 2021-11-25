787460c32cfb1257e0b591b0d383aca726e21856
manav <manav@wednesday.is>
2021-11-25 19:35:33 +0530

    Start Screen initial commit.

---
+++ presentation/src/main/java/com/wednesday/template/presentation/weather/start/StartFragmentScreen.kt
@@ -0,0 +1,8 @@
+package com.wednesday.template.presentation.weather.start
+
+import com.wednesday.template.presentation.R
+import com.wednesday.template.presentation.screen.MainScreen
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+object StartFragmentScreen : MainScreen(R.id.startFragment)
---
+++ presentation/src/main/java/com/wednesday/template/presentation/weather/start/StartScreenState.kt
@@ -0,0 +1,13 @@
+package com.wednesday.template.presentation.weather.start
+
+import com.wednesday.template.presentation.base.UIList
+import com.wednesday.template.presentation.base.UIToolbar
+import com.wednesday.template.presentation.screen.MainScreenState
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class StartScreenState(
+    override val toolbar: UIToolbar,
+    override val showLoading: Boolean,
+    val favoriteCitiesList: UIList = UIList()
+) : MainScreenState
