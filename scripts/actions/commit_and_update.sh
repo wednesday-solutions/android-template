9da27c8ca9306e4eed4781d157e238db84fc08f3
gtxtreme <gaurav.thakkar@wednesday.is>
2022-02-14 16:29:35 +0530

    Add step to upload app to AppCenter

--- scripts/actions/commit_and_update.sh
+++ scripts/actions/commit_and_update.sh
@@ -10,7 +10,7 @@ git config user.email "william-the-bot@wednesday.is"
 git config user.name "William The Bot"
 
 git add $GITHUB_WORKSPACE/app/app.gradle
-git commit -m "Bumped versionCode by 1"
+git commit -m "Bumped versionCode by 1 [skip ci]" # skip ci actually skips the ci
 
 CURRENT_BRANCH=$(git branch --show-current)
 git push origin $CURRENT_BRANCH
