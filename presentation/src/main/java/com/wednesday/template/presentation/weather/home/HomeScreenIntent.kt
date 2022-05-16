8714e1bda4fe4c410f130ef3b0e15f40c8f48376
Shounak Mulay <shounakmulay@gmail.com>
2022-05-16 12:07:57 +0530

    Fix formatting

--- presentation/src/main/java/com/wednesday/template/presentation/weather/home/HomeScreenIntent.kt
+++ presentation/src/main/java/com/wednesday/template/presentation/weather/home/HomeScreenIntent.kt
@@ -6,7 +6,7 @@ sealed interface HomeScreenIntent : Intent {
 
     object Search : HomeScreenIntent
 
-    object Loading: HomeScreenIntent
-    object Loading2: HomeScreenIntent
-    object Loading3: HomeScreenIntent
+    object Loading : HomeScreenIntent
+    object Loading2 : HomeScreenIntent
+    object Loading3 : HomeScreenIntent
 }
