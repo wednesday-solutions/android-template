9807394bcc29c1895d6359a0bf092e0a75c015f8
gtxtreme <gaurav.thakkar@wednesday.is>
2022-06-10 11:04:29 +0530

    Add Montserrat Font Family with Google Fonts

--- presentation/src/main/java/com/wednesday/template/presentation/base/theme/Type.kt
+++ presentation/src/main/java/com/wednesday/template/presentation/base/theme/Type.kt
@@ -1,116 +1,171 @@
 package com.wednesday.template.presentation.base.theme
+
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
-// Replace with your font locations
-val Roboto = FontFamily.Default
+import com.wednesday.template.presentation.R
+
+
+//@OptIn(ExperimentalTextApi::class)
+//val MontserratFontFamily = getMontserratFontFamily(
+//    name = "Roboto", weights = listOf<FontWeight>(
+//        FontWeight.Normal,
+//        FontWeight.Medium,
+//    )
+//)
+
+@OptIn(ExperimentalTextApi::class)
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+@OptIn(ExperimentalTextApi::class)
+val MontserratFont = GoogleFont(name = "Montserrat")
+
+@OptIn(ExperimentalTextApi::class)
+val MontserratFontFamily = FontFamily(
+    Font(googleFont = MontserratFont, fontProvider = provider),
+    Font(googleFont = MontserratFont, fontProvider = provider, weight = FontWeight.Light),
+    Font(googleFont = MontserratFont, fontProvider = provider, weight = FontWeight.Medium),
+    Font(googleFont = MontserratFont, fontProvider = provider, weight = FontWeight.SemiBold),
+)
+
 
 val AppTypography = Typography(
     displayLarge = TextStyle(
-        fontFamily = Roboto,
-        fontWeight = FontWeight.W400,
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 57.sp,
         lineHeight = 64.sp,
         letterSpacing = (-0.25).sp,
     ),
     displayMedium = TextStyle(
-        fontFamily = Roboto,
-        fontWeight = FontWeight.W400,
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 45.sp,
         lineHeight = 52.sp,
         letterSpacing = 0.sp,
     ),
     displaySmall = TextStyle(
-        fontFamily = Roboto,
-        fontWeight = FontWeight.W400,
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 36.sp,
         lineHeight = 44.sp,
         letterSpacing = 0.sp,
     ),
     headlineLarge = TextStyle(
-        fontFamily = Roboto,
-        fontWeight = FontWeight.W400,
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 32.sp,
         lineHeight = 40.sp,
         letterSpacing = 0.sp,
     ),
     headlineMedium = TextStyle(
-        fontFamily = Roboto,
-        fontWeight = FontWeight.W400,
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 28.sp,
         lineHeight = 36.sp,
         letterSpacing = 0.sp,
     ),
     headlineSmall = TextStyle(
-        fontFamily = Roboto,
-        fontWeight = FontWeight.W400,
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.sp,
     ),
     titleLarge = TextStyle(
-        fontFamily = Roboto,
-        fontWeight = FontWeight.W400,
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp,
     ),
     titleMedium = TextStyle(
-        fontFamily = Roboto,
+        fontFamily = MontserratFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.1.sp,
     ),
     titleSmall = TextStyle(
-        fontFamily = Roboto,
+        fontFamily = MontserratFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp,
     ),
     labelLarge = TextStyle(
-        fontFamily = Roboto,
+        fontFamily = MontserratFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp,
     ),
     bodyLarge = TextStyle(
-        fontFamily = Roboto,
-        fontWeight = FontWeight.W400,
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp,
     ),
     bodyMedium = TextStyle(
-        fontFamily = Roboto,
-        fontWeight = FontWeight.W400,
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.25.sp,
     ),
     bodySmall = TextStyle(
-        fontFamily = Roboto,
-        fontWeight = FontWeight.W400,
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.4.sp,
     ),
     labelMedium = TextStyle(
-        fontFamily = Roboto,
+        fontFamily = MontserratFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp,
     ),
     labelSmall = TextStyle(
-        fontFamily = Roboto,
+        fontFamily = MontserratFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp,
     ),
-)
+
+    )
+
+//Use Roboto as the fallback on stock Android, in case we fail to load the custom font,
+//Default could also use the system specific font on custom skins like MIUI, Oxygen OS etc
+
+//@OptIn(ExperimentalTextApi::class)
+//private fun getMontserratFontFamily(
+//    name: String,
+//    provider: GoogleFont.Provider = provider,
+//    weights: List<FontWeight>
+//): FontFamily {
+//    return FontFamily(
+//        fonts = weights.map {
+//            Font(googleFont = GoogleFont(name), fontProvider = provider, weight = it)
+//        }
+//    )
+//}
+
+//@OptIn(ExperimentalTextApi::class)
+//val googleFontProvider = GoogleFont.Provider(
+//    providerAuthority = "com.google.android.gms.fonts",
+//    providerPackage = "com.google.android.gms",
+//    certificates = R.array.com_google_android_gms_fonts_certs
+//)
